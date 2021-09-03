4ce0b4f6f1df29fe138129472491a66b94c24ada
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-03 09:41:28 -0600

    added paths, but not working yet

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,24 @@
 const express = require("express")
+const json = require("./db/db.json")
 
 const PORT = 3001;
 const app = express()
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
 });
+

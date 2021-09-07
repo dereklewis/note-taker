e5d54a7f21b66842a6033933751a4570b04f8523
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-07 16:18:30 -0600

    routes to html werefixed and uniqid was installed

--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,24 @@
-const express = require("express")
-const json = require("./db/db.json")
+const express = require("express");
+const json = require("./db/db.json");
+const path = require("path");
+const uniqid = require("uniqid");
 
 const PORT = 3001;
-const app = express()
+const app = express();
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
-});
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "public/notes.html")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
 
+app.get("*", (req, res) => 
+    res.sendFile(path.join(__dirname, "public/index.html")));
 
 
 
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
-});
 
+
+app.listen(PORT, () => 
+    console.log(`Server is listening on ${PORT}`));

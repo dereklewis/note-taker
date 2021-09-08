a0ad8a44f5d0d6da3983a3c085d6fb21432b58f3
Derek Lewis <derek.lewis.den@outlook.com>
2021-09-08 16:48:05 -0600

    get request and post requests have benn added

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const json = require("./db/db.json");
 const path = require("path");
 const uniqid = require("uniqid");
+const fs = require("fs");
 
 const PORT = 3001;
 const app = express();
@@ -11,6 +11,28 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+app.get("/api/notes", (req, res) => {
+
+try {
+    const data = fs.readFileSync("./db/db.json", "utf8");
+    
+    res.json(JSON.parse(data));
+} catch (error) {
+    res.json(error);
+}
+});
+
+app.post("/api/notes", (req, res) => {
+    const note = fs.readFileSync("./db/db.json", "utf8");
+    const noteParsed = JSON.parse(note); 
+    noteParsed.push(req.body);
+    console.log(noteParsed);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteParsed));
+
+    res.send("Your note has been sent");
+
+})
+
 app.get("/notes", (req, res) => 
     res.sendFile(path.join(__dirname, "public/notes.html")));
 
@@ -18,9 +40,9 @@ app.get("/notes", (req, res) =>
 app.get("*", (req, res) => 
     res.sendFile(path.join(__dirname, "public/index.html")));
 
-const getNotes = async () => {
-    const 
-}
+
+
+
 
 
 
